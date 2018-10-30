22aa6001dc344285964d412acbdbc5acea0b6be9
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-10-29 21:29:06 -0300

    grouping models to one folder

--- LdaMalletHandler.py
+++ LdaMalletHandler.py
@@ -12,17 +12,17 @@ class LdaMalletHandler:
         self.model_name = model_name
         self.dictionary = Dictionary(corpus)
         corpus_bow = [self.dictionary.doc2bow(text) for text in corpus]
-        os.makedirs(model_name, exist_ok=True )
-        self.model = LdaMallet(self.mallet_path, corpus_bow, num_topics=num_topics,id2word=self.dictionary, workers=workers, prefix="./"+model_name+"/")
+        os.makedirs("models/"+model_name, exist_ok=True )
+        self.model = LdaMallet(self.mallet_path, corpus_bow, num_topics=num_topics,id2word=self.dictionary, workers=workers, prefix="./models/"+model_name+"/")
 
     def save_model(self):
-        self.model.save(self.model_name+"/model.model")
-        self.dictionary.save(self.model_name+"/dict.dict")
+        self.model.save("models/"+self.model_name+"/model.model")
+        self.dictionary.save("models/"+self.model_name+"/dict.dict")
 
     def load_model(self, model_name):
         self.model_name = model_name
-        self.dictionary  = corpora.Dictionary.load(self.model_name+"/dict.dict")
-        self.model = LdaMallet.load(self.model_name+"/model.model") 
+        self.dictionary  = corpora.Dictionary.load("models/"+self.model_name+"/dict.dict")
+        self.model = LdaMallet.load("models/"+self.model_name+"/model.model") 
 
     def ext_doc_topics(self, ext_doc):
         doc_bow = self.dictionary.doc2bow(ext_doc)
