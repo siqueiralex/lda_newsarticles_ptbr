accaa7a29b787b5c13bc434777e10bd1e38070ab
Alex Siqueira Lacerda <Lequizi@gmail.com>
2018-11-22 15:46:11 -0200

    last changes

--- LdaMalletHandler.py
+++ LdaMalletHandler.py
@@ -1,29 +1,77 @@
 import os
+import numpy as np
+from scipy.spatial.distance import cosine
 from gensim.models.wrappers import LdaMallet
 from gensim import corpora
 from gensim.corpora.dictionary import Dictionary
-from DocumentRetriever import DocumentRetriever
+
+class DocumentRetriever:
+    def __init__(self, path):
+        self.path = path
+        arr = []
+        lines = open(path, "r").read().splitlines()
+        for line in lines:
+            arr.append(line.split()[2:])
+        self.matrix = np.array(arr, dtype=np.float64)
+    
+    def n_most_similar(self, doc, n=3, metric='cosine'):
+        result = []
+        if (metric=='cosine'):
+            distances = []
+            i = 0;
+            for item in self.matrix:
+                distances.append([i,cosine(doc, item)])
+                i+=1
+            distances.sort(key=lambda x: x[1])  
+            for rank in distances[:n]:
+                result.append(rank[0])
+        return result
+    
+    def doc_topics(self, doc_idx, sorted=True):
+        topics = []
+        i = 0;
+        for topic in self.matrix[doc_idx]:
+            topics.append([i,topic])
+            i+=1
+        topics.sort(key=lambda x: x[1], reverse=True)
+        return topics
+        
 
 class LdaMalletHandler:
     def __init__(self, mallet_path):
         self.mallet_path = mallet_path
 
-    def run_model(self, model_name, corpus, num_topics, workers=8, iterations=1000):
+    def run_model(self, model_name, corpus, **kwargs):
         self.model_name = model_name
         self.dictionary = Dictionary(corpus)
         corpus_bow = [self.dictionary.doc2bow(text) for text in corpus]
-        os.makedirs("models/"+model_name, exist_ok=True )
-        self.model = LdaMallet(self.mallet_path, corpus_bow, num_topics=num_topics,id2word=self.dictionary, workers=workers, prefix="./models/"+model_name+"/", iterations=iterations)
+        os.makedirs("ldamodels/"+model_name, exist_ok=True )
+        self.model = LdaMallet(self.mallet_path, corpus_bow, id2word=self.dictionary, prefix="./ldamodels/"+model_name+"/", **kwargs)
 
-    def save_model(self):
-        self.model.save("models/"+self.model_name+"/model.model")
-        self.dictionary.save("models/"+self.model_name+"/dict.dict")
+    def save_model(self, corpus_path=None):
+        if(corpus_path!=None):
+            file = open("ldamodels/"+self.model_name+"/info.info", "w")
+            file.write(corpus_path+'\n')
+            file.close()
+        self.model.save("ldamodels/"+self.model_name+"/model.model")
+        self.dictionary.save("ldamodels/"+self.model_name+"/dict.dict")
 
     def load_model(self, model_name):
         self.model_name = model_name
-        self.dictionary  = corpora.Dictionary.load("models/"+self.model_name+"/dict.dict")
-        self.model = LdaMallet.load("models/"+self.model_name+"/model.model") 
-
+        self.dictionary  = corpora.Dictionary.load("ldamodels/"+self.model_name+"/dict.dict")
+        self.model = LdaMallet.load("ldamodels/"+self.model_name+"/model.model")
+        self.model.mallet_path = self.mallet_path
+        
+        #file = open("ldamodels/"+self.model_name+"/info.info", "r")
+        #args = file.read().splitlines()
+        #file.close()
+        #self.corpus_path=args[0]
+    
+    def doc_topics(self, doc_idx):
+        if(not hasattr(self, 'doc_retriever')):
+            self.doc_retriever =  DocumentRetriever(self.model.fdoctopics())
+        return self.doc_retriever.doc_topics(doc_idx)    
+    
     def ext_doc_topics(self, ext_doc):
         doc_bow = self.dictionary.doc2bow(ext_doc)
         doc_topics = self.model[doc_bow]
@@ -41,6 +89,14 @@ class LdaMalletHandler:
         most_similar = self.doc_retriever.n_most_similar(topics, n=n, metric=metric)    
         return most_similar
 
+    def n_most_representative(self, topic, n=3):
+         if(not hasattr(self, 'doc_retriever')):
+            self.doc_retriever =  DocumentRetriever(self.model.fdoctopics())
+         topics = np.zeros(self.model.num_topics)
+         topics[topic]=1
+         most_similar = self.doc_retriever.n_most_similar(topics, n=n)
+         return most_similar
+        
     def get_string_topics(self, num_topics=-1, num_words=10):
         if(num_topics==-1):
             num_topics = self.model.num_topics 
